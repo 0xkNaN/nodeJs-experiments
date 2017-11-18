74fc8d9edf95ff850603762561386e2a8205a265
Hassen Rmili <hrmili@bulldozerinc.com>
2017-11-18 15:52:59 +0100

    #EDIT: Edit gitignore

--- recap/routes/gamesRoutes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var gamesRouter = express.Router();
-
-gamesRouter.get('/', function(req, res) {
-  res.render('games');
-});
-
-module.exports = gamesRouter;
--- recap/routes/languagesRoutes.js
+++
@@ -1,9 +0,0 @@
-
-var express = require('express');
-var languagesRoute = express.Router();
-
-languagesRoute.get('/', function(req, res) {
-  res.render('languages');
-});
-
-module.exports = languagesRoute;
